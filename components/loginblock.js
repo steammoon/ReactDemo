9cc5a9a019bd550fa36b4b50d588a472b435b843
steammoon <39254660+steammoon@users.noreply.github.com>
2019-05-31 14:30:05 +0800

    react_demo_001_clear

---
+++ components/loginblock.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class LoginBlock extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-1 col-xs-offset-11">
+                        <div>登陆窗口</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
